1a3dcd8007bfcb641fc18af209e4e5cbafd5c02e
AbhishekGaire <abhisekgaire7@gmail.com>
2024-12-05 15:20:23 +0545

    fix: ejs error

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 // app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public"))); //Static files
 
